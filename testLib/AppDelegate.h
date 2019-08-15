12c8ce1dd79609887b922b83f4444eccaa091d9d
zl <>
2019-08-15 22:20:57 +0800

    注释

---
+++ testLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testLib
+//
+//  Created by zl on 2019/8/15.
+//  Copyright © 2019 zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testLib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testLib
+//
+//  Created by zl on 2019/8/15.
+//  Copyright © 2019 zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
